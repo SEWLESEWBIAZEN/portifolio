c8303ae4f7955e666de7c08aaa404fb632addbbb
Sewlesew Biazen <sewlesew.biazen@addisbanksc.local>
2024-04-15 14:53:21 +0300

    correcting spelling errors from hard-coded text

--- src/components/projects/Index.jsx
+++ src/components/projects/Index.jsx
@@ -7,7 +7,7 @@ const Projects = () => {
     <div className='container justify-content-center d-flex flex-wrap'>
         <Project        
         name="Payment Method named as HooBank UI/UX Website"
-        description="A pixel-perfet ui/ux website made with react, tailwind css and html5. 
+        description="A pixel-perfect ui/ux website made with react, tailwind css and html5. 
         It has many sections as full-fledged payment system website user experience. HooBank is a digital payment system 
         you can use to pay fast and efficiently."
         githubLink="https://github.com/SEWLESEWBIAZEN/pay-better.github.io"
@@ -16,14 +16,14 @@ const Projects = () => {
         />
         <Project        
         name="Restaurant UI/UX Website"
-        description="A pixel-perfet ui/ux website made with react, custom css and html5. It has many sections as full-fledged restaurant ui/ux website"
+        description="A pixel-perfect ui/ux website made with react, custom css and html5. It has many sections as full-fledged restaurant ui/ux website"
         githubLink="https://github.com/SEWLESEWBIAZEN/sew-restaurant.github.io"
         websiteLink="https://sew-restaurant.netlify.app"
         imgUrl={restaurant}
         />
          <Project        
         name="GPT-3 UI/UX website"
-        description="A pixel-perfet ui/ux website made with react, custom css and html5. It has many sections as full-fledged gpt-3 ui/ux website.It has a complete graphics and images."
+        description="A pixel-perfect ui/ux website made with react, custom css and html5. It has many sections as full-fledged gpt-3 ui/ux website.It has a complete graphics and images."
         githubLink="https://github.com/SEWLESEWBIAZEN/gpt3_sewdevs.github.io"
         websiteLink="https://gpt-three-3.netlify.app"
         imgUrl={gpt3}
@@ -39,7 +39,7 @@ const Projects = () => {
        
         <Project        
         name="Amhara Bank Internet banking Demo website"
-        description="A pixel-perfet ui/ux website made with react, Material UI and html5. It has many sections as full-fledged digital payment system user interface. It is user friendly and responsive for labtop, tablet or smart phones"
+        description="A pixel-perfect ui/ux website made with react, Material UI and html5. It has many sections as full-fledged digital payment system user interface. It is user friendly and responsive for labtop, tablet or smart phones"
         githubLink="https://github.com/SEWLESEWBIAZEN/Amhara-Bank-Internet-Banking"
         websiteLink="https://abaib.netlify.app"
         imgUrl={abaib}
